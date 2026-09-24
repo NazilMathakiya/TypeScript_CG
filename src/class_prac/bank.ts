2c5144f4b0462e2d8c1bb5169bc1ebdf9f77819e
Mahammadnazil <mathakiyanazil03@gmail.com>
2026-09-24 11:02:48 +0530

    Prac

---
+++ src/class_prac/bank.ts
@@ -0,0 +1,53 @@
+class BankAccount {
+    static TotalBankHoldingAmount: number = 0
+    static TotalAcountHolder: number = 0
+
+    constructor(
+        public id: number,
+        public name: string,
+        public amount: number
+    ){
+        console.log("constructor executed!!");
+        BankAccount.TotalBankHoldingAmount += amount
+        BankAccount.TotalAcountHolder++
+    }
+
+    static{
+        console.log("This is static block");
+        
+    }
+
+    credit(amount:number){
+        BankAccount.TotalBankHoldingAmount += amount
+        this.amount += amount
+    }
+    debit(amount:number){
+        BankAccount.TotalBankHoldingAmount -= amount
+        this.amount -= amount
+    }
+
+    getAccountInfo(){
+        console.log(`name: ${this.name}\nMy_Balance: ${this.amount}`)
+    }
+    static bankDetail(){
+        console.log(`total account: ${BankAccount.TotalAcountHolder}\ntotal bank holding:${BankAccount.TotalBankHoldingAmount}`);
+        
+    }
+}
+
+// const b1 = new BankAccount(1, "nazil", 5000)
+// const b2 = new BankAccount(1, "nazil", 5000)
+
+// b1.credit(2000)
+// b2.credit(1500)
+
+// b1.getAccountInfo()
+// b2.getAccountInfo()
+// // 
+// b1.debit(4000)
+// b2.debit(1200)
+
+// b1.getAccountInfo()
+// b2.getAccountInfo()
+
+// BankAccount.bankDetail
